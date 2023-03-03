7aa5c8731cbe0e752006b479fd4a18564a8f85a5
Gabriel <gabrielfranz31@gmail.com>
2023-03-03 10:15:21 +0100

    Co-authored-by: sunset-wolf <sunset-wolf@users.noreply.github.com> Co-authored-by: Bischof Till <till.bischof@edu.tbz.ch>

---
+++ Projekt/register.php
@@ -0,0 +1,21 @@
+<?php 
+include_once 'includes/connect-db.php';
+?>
+
+<!DOCTYPE html>
+
+<?php
+include_once 'everywhere/header.php'
+?>
+
+<body>
+    <?php
+    include_once 'everywhere/navbar.php'
+    ?>
+    
+    <!-- Insert Register Code here-->
+
+    <?php
+    include_once 'everywhere/footer.php'
+    ?>
+</body>

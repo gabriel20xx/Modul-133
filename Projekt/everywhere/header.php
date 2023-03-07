7e19baacb7cae55d213c1c587ffc8abf3c287bf8
Gabriel <gabrielfranz31@gmail.com>
2023-03-07 08:22:04 +0100

    Updated

--- Projekt/everywhere/header.php
+++ Projekt/everywhere/header.php
@@ -1,10 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="icon" href="../icon/favicon.png">
-    <title>Beschte Website</title>
+    <title>Geschichten aus der TBZ</title>
     <!-- Insert head Code here -->
 </head>
 <body>
+
--- Projekt/login.php
+++ Projekt/login.php
@@ -1,22 +1,58 @@
 <?php 
-include_once 'includes/connect-db.php';
+#include_once 'includes/connect-db.php';
+include_once 'everywhere/header.php';
 ?>
 
-<!DOCTYPE html>
+<body class="text-center login-page">
 
-<?php
-include_once 'everywhere/header.php'
-?>
-
-<body>
-    <?php
-    include_once 'everywhere/navbar.php'
-    ?>
-    
     <!-- Insert Login Code here-->
-    <h1>Dies ist der Loginbereich</h1>
+    <main class="form-signin w-100 m-auto">
+    <form action="/login.php" method="post">
+        <img class="mb-4" src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="logo" width="72" height="57">
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div class="form-floating">
+        <input type="email" class="form-control form-first" id="email" placeholder="name@example.com">
+        <label for="email">Email address</label>
+        </div>
+        <div class="form-floating">
+        <input type="password" class="form-control form-last" id="password" placeholder="Password">
+        <label for="password">Password</label>
+        </div>
+
+        <div class="checkbox mb-3">
+        <label>
+            <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        </div>
+        <button class="w-100 btn btn-lg mb-3 btn-primary" type="submit">Sign in</button>
+        <a href="register.php">
+        <div class="w-100 btn btn-lg btn-secondary">Go to register</div>
+        </a>
+        <p class="mt-5 mb-3 text-muted">&copy; Gabriel, Cornel, Till 2023</p>
+    </form>
+    <div class="errors">
+    <?php
+    if (isset($_GET["error"])) {
+        if ($_GET["error"] == "emptyinput") {
+            echo 
+            '<div class="alert alert-danger" role="alert">
+            Fill in all fields!
+            </div>';
+        }
+        else if ($_GET["error"] == "wronglogin") {
+            echo 
+            '<div class="alert alert-danger" role="alert">
+            Incorrect Login information!
+            </div>';
+        }
+    }
+    ?>
+    </div>
+</main>
+
 
     <?php
-    include_once 'everywhere/footer.php'
+    include_once 'everywhere/footer.php';
     ?>
 </body>

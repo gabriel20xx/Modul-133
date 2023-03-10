1e114c6206baa018b72a6b875777bf21972159c5
Gabriel <gabrielfranz31@gmail.com>
2023-03-10 09:04:02 +0100

    Commit
    Co-authored-by: Bischof Till <till.bischof@edu.tbz.ch>

--- everywhere/navbar.php
+++ everywhere/navbar.php
@@ -12,7 +12,7 @@
           <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../profile.php">Forum</a>
+          <a class="nav-link" href="../forum.php">Forum</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="../about.php">About</a>

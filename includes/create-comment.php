1934c7d70ed2e0fbdf9a848fa62b6d6d2e1f2736
Gabriel <gabrielfranz31@gmail.com>
2023-03-15 08:56:53 +0100

    Add Comment functionality

---
+++ includes/create-comment.php
@@ -0,0 +1,25 @@
+<?php 
+if (isset($_POST["submit"])) {
+    $description = $_POST["description"];
+    $blog_uuid = $_POST["blog_uuid"];
+
+    require_once 'connect-db.php';
+    require_once 'functions.php';
+
+    if (emptyInputCreateComment($$description) !== false) {
+        header("location: ../blogs/$blog_uuid.php?error=emptyinput");
+        exit();
+    };
+
+    if (checkUserLogin() !== false) {
+        exit();
+    };
+
+    $uuid = uuid_create(UUID_TYPE_RANDOM);
+
+    createComment($conn, $uuid, $description, $blog_uuid);
+
+} else {
+    header("location: ../forum.php");
+    exit();
+}

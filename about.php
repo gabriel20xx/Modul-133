ffa6674412dc169d3934372ee8f17f9e2b9e540a
Gabriel <gabrielfranz31@gmail.com>
2023-03-17 10:09:11 +0100

    Commit

--- about.php
+++ about.php
@@ -8,7 +8,36 @@ include_once 'everywhere/header.php'
     include_once 'everywhere/navbar.php'
     ?>
 
+<div class="container">
+    <h1>About Us</h1>
+    <p>Our company is committed to providing high-quality products and services to our customers.</p>
     
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Our Mission</h2>
+        <p>We strive to be the best in our industry by delivering exceptional products and services to our customers.</p>
+      </div>
+      
+      <div class="col-md-6">
+        <h2>Our Vision</h2>
+        <p>We aim to become a leader in our industry by constantly innovating and improving our products and services.</p>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-md-12">
+        <h2>Our Team</h2>
+        <p>Meet the people behind our success:</p>
+        
+        <ul class="list-group">
+          <li class="list-group-item">John Doe - CEO</li>
+          <li class="list-group-item">Jane Doe - COO</li>
+          <li class="list-group-item">Mark Smith - CFO</li>
+          <li class="list-group-item">Lisa Brown - CMO</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 
       
       <!-- Footer section -->

70dab2f81490f76e8481ea27d936fe55b634ea28
Gabriel <gabrielfranz31@gmail.com>
2023-03-17 11:40:38 +0100

    Updates

--- everywhere/navbar.php
+++ everywhere/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-sm bg-body-tertiary">
   <div class="container">
     <a class="navbar-brand" href="../index.php">
-    <img class="mb-4" src="pictures/LogoGCT.png" alt="LogoGTC" height="100%">
+    <img class="mb-4" src="pictures/LogoGCT.png" alt="LogoGTC" height="30px">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>

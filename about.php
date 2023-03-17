6256c89af74a392e0fb82330ae3e465d9c9fa387
Gabriel <gabrielfranz31@gmail.com>
2023-03-17 10:05:33 +0100

    Updated

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+include_once 'includes/connect-db.php';
+include_once 'everywhere/header.php'
+?>
+
+<body>
+    <?php
+    include_once 'everywhere/navbar.php'
+    ?>
+
+    
+
+      
+      <!-- Footer section -->
+    <?php
+    include_once 'everywhere/footer.php'
+    ?>
+</body>

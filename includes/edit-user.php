65847f81cc9c83891e2d59848ed7c2a0ecd78ef6
Gabriel <gabrielfranz31@gmail.com>
2023-03-31 09:22:15 +0200

    added edit functionality

--- includes/edit-user.php
+++ includes/edit-user.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_POST['submit'])) {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $uuid = $_POST['uuid'];
     $username = $_POST['username'];
     $email = $_POST['email'];

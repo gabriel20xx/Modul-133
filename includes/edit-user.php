ea1504d52847506593393b55e9adda3e9c174b87
Gabriel <gabrielfranz31@gmail.com>
2023-04-14 08:16:42 +0200

    update

--- includes/edit-user.php
+++ includes/edit-user.php
@@ -1,17 +1,19 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $uuid = $_POST['uuid'];
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    // TODO: validate and sanitize the data
     require_once 'connect-db.php';
     require_once 'functions.php';
 
     updateUser($conn, $uuid, $username, $email, $password);
-
-//} else {
-   echo "<p>$uuid dies ist die uuid2</p>";
-    //header("location: ../profile/$uuid.php");
-    exit();
-//}
+    // TODO: update user data in database using the retrieved form data
+    echo "<p>$uuid dies ist die uuid2</p>";
+    // Redirect user to profile page
+    header('Location: ../profile.php');
+    exit;
+}

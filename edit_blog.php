a5e5ece24c678f5ce65535a60fa02159a449fda1
Gabriel <gabrielfranz31@gmail.com>
2023-04-14 09:02:13 +0200

    fixes

--- edit_blog.php
+++ edit_blog.php
@@ -7,19 +7,36 @@ include_once 'everywhere/header.php'
     <?php
     include_once 'everywhere/navbar.php'
     ?>
+    <?php
+        if (isset($_GET["edit"])) {
+            $uuid = $_GET["edit"];
+            $sql = "SELECT * FROM blogs WHERE uuid = '$uuid'";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+
+            if ($resultCheck > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $uuid = $row['uuid'];
+                    $title = $row['title'];
+                    $description = $row['description'];
+    
+                }
+            }
+        }
+    ?>
 
     <!-- Insert Blogpage Code here-->
     <div class="container">
         <form action="includes/edit-blog.php" method="post">
             <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-                <input type="text" name="title" class="form-control" id="title" placeholder="Insert good title here">
+                <input type="text" name="title" class="form-control" id="title" placeholder="Insert good title here" value="<?php echo $title ?>">
             </div>
             <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-                <textarea class="form-control" name="description" id="description" rows="10" placeholder="Insert description here"></textarea>
+                <textarea class="form-control" name="description" id="description" rows="10" placeholder="Insert description here"><?php echo $description ?></textarea>
             </div>
-            <button class="btn btn-lg mb-3 btn-primary" name="submit" type="submit">Create Blog</button>
+            <button class="btn btn-lg mb-3 btn-primary" name="submit" type="submit">Update Blog</button>
         </form>
     </div>
 

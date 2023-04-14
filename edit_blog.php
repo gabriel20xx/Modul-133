71cf565c66621d0eb61f2f2fdd0b06e3bda731a2
Gabriel <gabrielfranz31@gmail.com>
2023-04-14 11:36:23 +0200

    fixes

--- edit_blog.php
+++ edit_blog.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once 'includes/connect-db.php';
 include_once 'everywhere/header.php'
 ?>
@@ -8,21 +8,21 @@ include_once 'everywhere/header.php'
     include_once 'everywhere/navbar.php'
     ?>
     <?php
-        if (isset($_GET["edit"])) {
-            $uuid = $_GET["edit"];
-            $sql = "SELECT * FROM blogs WHERE uuid = '$uuid'";
-            $result = mysqli_query($conn, $sql);
-            $resultCheck = mysqli_num_rows($result);
+    if (isset($_GET["edit"])) {
+        $uuid = $_GET["edit"];
+        $sql = "SELECT * FROM blogs WHERE uuid = '$uuid'";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
 
-            if ($resultCheck > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $uuid = $row['uuid'];
-                    $title = $row['title'];
-                    $description = $row['description'];
-                    $category = $row['category'];
-                }
+        if ($resultCheck > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $uuid = $row['uuid'];
+                $title = $row['title'];
+                $description = $row['description'];
+                $category_id = $row['category_id'];
             }
         }
+    }
     ?>
 
     <!-- Insert Blogpage Code here-->
@@ -33,6 +33,15 @@ include_once 'everywhere/header.php'
                 <label for="category" class="form-label">Category</label>
                 <select class="form-select" name="category" id="category" aria-label="Default select example">
                     <?php
+                    $sql = "SELECT * FROM categories WHERE id = '$category_id'";
+                    $result = mysqli_query($conn, $sql);
+                    $resultCheck = mysqli_num_rows($result);
+
+                    if ($resultCheck > 0) {
+                        $row = mysqli_fetch_assoc($result);
+                        $category = $row['name'];
+                    }
+
                     echo "<option selected>$category</option>";
                     $sql = "SELECT COUNT(*) as count FROM categories";
                     $result = mysqli_query($conn, $sql);
@@ -61,11 +70,11 @@ include_once 'everywhere/header.php'
             </div>
             <input type='hidden' name='uuid' value='<?php echo $uuid ?>'>
             <div class="mb-3">
-            <label for="title" class="form-label">Title</label>
+                <label for="title" class="form-label">Title</label>
                 <input type="text" name="title" class="form-control" id="title" value="<?php echo $title ?>">
             </div>
             <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
+                <label for="description" class="form-label">Description</label>
                 <textarea class="form-control" name="description" id="description" rows="10"><?php echo $description ?></textarea>
             </div>
             <button class="btn btn-lg mb-3 btn-primary" name="submit" type="submit">Update Blog</button>

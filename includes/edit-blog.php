731ac15ab29d42091dcda2e98fa09e863c836d73
Gabriel <gabrielfranz31@gmail.com>
2023-04-14 11:20:44 +0200

    test

--- includes/edit-blog.php
+++ includes/edit-blog.php
@@ -7,7 +7,7 @@ if (isset($_POST["submit"])) {
     $description = $_POST["description"];
     $category = $_POST["category"];
 
-    header("Location: ../forum.php?$uuid$title$description$category");
+    header("Location: ../forum.php?bla$uuid$title$description$category");
 
     require_once 'connect-db.php';
     require_once 'functions.php';

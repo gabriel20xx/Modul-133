f1cd40f483bc35fb60576d01acae90cc66d4652b
Gabriel <gabrielfranz31@gmail.com>
2023-04-18 13:30:56 +0200

    header fixes

--- everywhere/navbar.php
+++ everywhere/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-sm bg-body-tertiary">
   <div class="container">
     <a class="navbar-brand" href="../index.php" height="42px">
-    <img class="mb-4" src="pictures/LogoGCT.png" alt="LogoGTC" height="42px">
+    <img class="mb-4" src="../pictures/LogoGCT.png" alt="LogoGTC" height="42px">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@
           <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../forum.php?page=1">Forum</a>
+          <a class="nav-link" href="../forum.php">Forum</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="../about.php">About</a>

5e49f3c85c9d9ef96d94b244726c033a69d0ddb4
Gabriel <gabrielfranz31@gmail.com>
2023-04-18 11:22:36 +0200

    test

--- forum.php
+++ forum.php
@@ -70,10 +70,10 @@ if (isset($_GET["page"])) {
             <?php
             if (isset($_SESSION["uuid"])) {
                 echo '    
-    <div class="text-center mt-5">
+/*     <div class="text-center mt-5"> */
     <div>
     <a class="btn btn-lg btn-success" href="new_blog.php" role="button">Create new post</a>
-    </div>    
+/*     </div>     */
     </div>';
             }
             ?>

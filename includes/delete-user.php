2bad184073d4b436e24b066e8c9aada9a1a18e9c
Gabriel <gabrielfranz31@gmail.com>
2023-04-19 09:58:52 +0200

    test

--- includes/delete-user.php
+++ includes/delete-user.php
@@ -7,6 +7,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require_once 'connect-db.php';
     require_once 'functions.php';
 
+    header("location: ../profile/nn.php?uuid=$uuid");
+
     deleteUser($conn, $uuid);
 
 } else {

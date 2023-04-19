eec4b966b8b10f01cfe0bc3bcec7a7da226f1f26
Gabriel <gabrielfranz31@gmail.com>
2023-04-19 16:20:14 +0200

    library changes

--- everywhere/footer.php
+++ everywhere/footer.php
@@ -7,11 +7,10 @@
   </div>
 </footer>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://unpkg.com/@popperjs/core@2.10.3/dist/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.3/umd/popper.min.js"></script>
 
 <!-- JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js" integrity="sha512-5i6jK1CQ2r5O5EaU5KjxwtWEnu8MFVYIjKu0lZgEjb6fbF7/IOHVKDq3pVFKxWYt8RW/gtODnTq3p9oT+iPxgg==" crossorigin="anonymous"></script>
 <script>
   const button = document.querySelector('.navbar-toggler');
   const menu = document.querySelector('.navbar-collapse');

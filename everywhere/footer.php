58d0bf0ccb163c1473c9b7b4af673dd8574cfac9
Gabriel <gabrielfranz31@gmail.com>
2023-04-19 16:21:36 +0200

    reverted previous commit

--- everywhere/footer.php
+++ everywhere/footer.php
@@ -1,56 +1,54 @@
 <footer>
-  <!-- Insert Footer Code here -->
-  <div class="row mt-3">
-    <div class="col-12 text-center">
-      <p>&copy; 2023 The GCT Corner. All rights reserved.</p>
+    <!-- Insert Footer Code here -->
+    <div class="row mt-3">
+        <div class="col-12 text-center">
+          <p>&copy; 2023 The GCT Corner. All rights reserved.</p>
+        </div>
     </div>
-  </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.3/umd/popper.min.js"></script>
-
-<!-- JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js" integrity="sha512-5i6jK1CQ2r5O5EaU5KjxwtWEnu8MFVYIjKu0lZgEjb6fbF7/IOHVKDq3pVFKxWYt8RW/gtODnTq3p9oT+iPxgg==" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script>
-  const button = document.querySelector('.navbar-toggler');
-  const menu = document.querySelector('.navbar-collapse');
+const button = document.querySelector('.navbar-toggler');
+const menu = document.querySelector('.navbar-collapse');
 
-  button.addEventListener('click', function() {
-    if (menu.classList.contains('show')) {
-      menu.classList.remove('show');
-    } else {
-      menu.classList.add('show');
-    }
-  });
-</script>
-<script>
-  // Gets current document name for example: /index.php 
-  // document.getElementById("footerr").innerHTML = window.location.pathname;
-  // Get the current page URL
-  var currentPage = window.location.pathname;
-
-  // Get all the links in the navbar
-  var navLinks = document.querySelectorAll('.navbar-nav a');
-
-  // Loop through each link and update the active class if it matches the current page URL
-  for (var i = 0; i < navLinks.length; i++) {
-    var link = navLinks[i];
-    if (link.pathname === currentPage) {
-      link.classList.add('active');
-    } else {
-      link.classList.remove('active');
-    }
+button.addEventListener('click', function() {
+  if (menu.classList.contains('show')) {
+    menu.classList.remove('show');
+  } else {
+    menu.classList.add('show');
   }
+});
 </script>
 <script>
-  document.addEventListener("DOMContentLoaded", function() {
+// Gets current document name for example: /index.php 
+// document.getElementById("footerr").innerHTML = window.location.pathname;
+// Get the current page URL
+var currentPage = window.location.pathname;
+
+// Get all the links in the navbar
+var navLinks = document.querySelectorAll('.navbar-nav a');
+
+// Loop through each link and update the active class if it matches the current page URL
+for (var i = 0; i < navLinks.length; i++) {
+  var link = navLinks[i];
+  if (link.pathname === currentPage) {
+    link.classList.add('active');
+  } else {
+    link.classList.remove('active');
+  }
+}
+
+</script>
+<script>
+document.addEventListener("DOMContentLoaded", function() {
     const myAlert = document.querySelector("#alertBox");
     const btnClose = myAlert.querySelector(".btn-close");
     btnClose.addEventListener("click", function() {
-      myAlert.classList.add("hide");
+        myAlert.classList.add("hide");
     });
-  });
+});
 </script>
 </body>
-
 </html>

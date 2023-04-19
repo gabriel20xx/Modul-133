9d0abde6fe612f0f13fe7ce7a65dc88a1f1a2fc1
Gabriel <gabrielfranz31@gmail.com>
2023-04-19 10:46:09 +0200

    navbar test

--- everywhere/navbar.php
+++ everywhere/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-sm bg-body-tertiary mb-4">
   <div class="container">
     <a class="navbar-brand" href="../index.php" height="42px">
-    <img class="mb-4" src="../pictures/LogoGCT.png" alt="LogoGTC" height="42px">
+      <img class="mb-4" src="../pictures/LogoGCT.png" alt="LogoGTC" height="42px">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+    <div class="collapse navbar-collapse justify-content-between justify-content-center text-center" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="../index.php">Home</a>

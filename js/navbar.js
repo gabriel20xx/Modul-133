a979e50118428599dc1412c11e6c4e9acd7e7594
Gabriel <gabrielfranz31@gmail.com>
2023-05-08 15:14:01 +0200

    fix

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,3 @@
-/* const button = document.querySelector(".navbar-toggler");
-const menu = document.querySelector(".navbar-collapse");
-
-button.addEventListener("click", function () {
-  if (menu.classList.contains("show")) {
-    menu.classList.remove("show");
-  } else {
-    menu.classList.add("show");
-  }
-});
-
- */
 var currentPage = window.location.pathname;
 var navLinks = document.querySelectorAll(".navbar-nav a");
 

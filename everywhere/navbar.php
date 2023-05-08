c038ab404b24c46f155a41bbf74c2794175d6e03
Gabriel <gabrielfranz31@gmail.com>
2023-05-08 14:53:36 +0200

    test navbar

--- everywhere/navbar.php
+++ everywhere/navbar.php
@@ -40,3 +40,42 @@
     </div>
   </div>
 </nav>
+
+<div class="container">
+  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <div class="col-md-3 mb-2 mb-md-0">
+      <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+        <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap">
+          <use xlink:href="#bootstrap"></use>
+        </svg>
+      </a>
+    </div>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+      <li><a href="../index.php" class="nav-link px-2 link-secondary">Home</a></li>
+      <li><a href="../forum.php" class="nav-link px-2">Forum</a></li>
+      <li><a href="../about.php" class="nav-link px-2">About</a></li>
+      <li><a href="../impressum.php" class="nav-link px-2">Impressum</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+      <?php if (isset($_SESSION["uuid"])) {
+        echo "
+        <a href='../includes/logout.php'>
+        <button type='button' class='btn btn-outline-primary me-2'>Logout</button>
+        </a>
+        <a href='../profiles/" . $_SESSION["uuid"] . ".php'>
+        <button type='button' class='btn btn-primary'>Profile</button>
+        </a>";
+      } else {
+        echo "
+        <a href='../includes/login.php'>
+        <button type='button' class='btn btn-outline-primary me-2'>Login</button>
+        </a>
+        <a href='../includes/register.php'>
+        <button type='button' class='btn btn-primary'>Register</button>
+        </a>";
+      } ?>
+    </div>
+  </header>
+</div>

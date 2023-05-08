5aa43c6a1d95f138541b6e72630ca5bbdd9e7992
Gabriel <gabrielfranz31@gmail.com>
2023-05-08 14:58:40 +0200

    navbar improvements

--- everywhere/navbar.php
+++ everywhere/navbar.php
@@ -44,14 +44,12 @@
 <div class="container">
   <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
     <div class="col-md-3 mb-2 mb-md-0">
-      <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-        <a class="navbar-brand" href="../index.php" height="42px">
-          <img src="../pictures/LogoGCT.png" alt="LogoGTC" height="42px">
-        </a>
+      <a class="navbar-brand" href="../index.php" class="d-inline-flex link-body-emphasis text-decoration-none"> height="42px">
+        <img src="../pictures/LogoGCT.png" alt="LogoGTC" height="42px">
       </a>
     </div>
 
-    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 text-decoration-none">
       <li class="nav-item"><a href="../index.php" class="nav-link px-2 link-secondary">Home</a></li>
       <li class="nav-item"><a href="../forum.php" class="nav-link px-2">Forum</a></li>
       <li class="nav-item"><a href="../about.php" class="nav-link px-2">About</a></li>

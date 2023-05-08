4dbbe314a1bbd9fed3d47e13d7e833cf216e794a
Gabriel <gabrielfranz31@gmail.com>
2023-05-08 14:11:11 +0200

    add impressum

---
+++ impressum.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'includes/connect-db.php';
+include_once 'everywhere/header.php';
+include_once 'everywhere/navbar.php';
+?>
+
+<div class="container my-5">
+    <h1 class="mb-4">Impressum</h1>
+    <p>Company Name: TheGCTCorner</p>
+    <p>Address: Thegctstreet 1</p>
+    <p>Telephone: +41 00 000 00 00</p>
+    <p>Email: info@thegctcorner.ch</p>
+    <p>Managing Director: Gabriel Franz</p>
+</div>
+
+<?php
+include_once 'everywhere/footer.php'
+?>

e9c769a8cfc1791dacc7b4d7cc456ee1aa7d9cc9
Gabriel <gabrielfranz31@gmail.com>
2023-05-08 14:16:47 +0200

    updated impressum

--- impressum.php
+++ impressum.php
@@ -4,14 +4,37 @@ include_once 'everywhere/header.php';
 include_once 'everywhere/navbar.php';
 ?>
 
-<div class="container my-5">
+<main>
+  <section class="container my-5">
     <h1 class="mb-4">Impressum</h1>
     <p>Company Name: TheGCTCorner</p>
     <p>Address: Thegctstreet 1</p>
     <p>Telephone: +41 00 000 00 00</p>
     <p>Email: info@thegctcorner.ch</p>
     <p>Managing Director: Gabriel Franz</p>
-</div>
+    <h2 class="my-4">Legal Disclaimer</h2>
+    <p>The information contained in this website is for general information purposes only. The information is provided by TheGCTCorner and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</p>
+    <p>In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.</p>
+    <h2 class="my-4">Privacy Policy</h2>
+    <p>TheGCTCorner is committed to protecting your privacy. We will only use the information that we collect about you lawfully (in accordance with the General Data Protection Regulation).</p>
+    <p>We may collect information about you for 2 reasons:</p>
+    <ul>
+      <li>To process your order</li>
+      <li>To provide you with the best possible service</li>
+    </ul>
+    <p>We will not e-mail you in the future unless you have given us your consent. We will give you the chance to refuse any marketing email from us in the future.</p>
+    <p>The type of information we may collect about you includes:</p>
+    <ul>
+      <li>Your name</li>
+      <li>Email address</li>
+    </ul>
+    <p>We will never collect sensitive information about you without your explicit consent.</p>
+    <p>The information we hold will be accurate and up to date. You can check the information that we hold about you by emailing us. If you find any inaccuracies we will delete or correct them promptly.</p>
+    <p>The personal information which we hold will be held securely in accordance with our internal security policy and the law.</p>
+    <h2 class="my-4">Copyright</h2>
+    <p>All content included on this site, such as text, graphics, logos, button icons, images, audio clips, digital downloads, data compilations, and software, is the property of [Insert Company Name Here] or its content suppliers and protected by international copyright laws. The compilation of all content on this site is the exclusive property of [Insert Company Name Here] and protected by international copyright laws.</p>
+  </section>
+</main>
 
 <?php
 include_once 'everywhere/footer.php'

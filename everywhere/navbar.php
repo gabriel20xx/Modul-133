e6cfe57894423f37ca795e56db51d03f3b91cbad
Gabriel <gabrielfranz31@gmail.com>
2023-05-08 14:55:00 +0200

    updated navbar

--- everywhere/navbar.php
+++ everywhere/navbar.php
@@ -45,9 +45,9 @@
   <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
     <div class="col-md-3 mb-2 mb-md-0">
       <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-        <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap">
-          <use xlink:href="#bootstrap"></use>
-        </svg>
+        <a class="navbar-brand" href="../index.php" height="42px">
+          <img src="../pictures/LogoGCT.png" alt="LogoGTC" height="42px">
+        </a>
       </a>
     </div>
 
